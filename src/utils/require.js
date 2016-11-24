cb60d92d6abe419c7633bdce57114b5206777da9
laggingreflex <laggingreflex@gmail.com>
2016-11-24 20:47:08 +0530

    fix: require

--- src/utils/require.js
+++ src/utils/require.js
@@ -1,57 +1,69 @@
 import assert from 'assert';
-import { resolve } from 'path';
+import { join } from 'path';
 import reqFrom from 'req-from';
 import log from '.../utils/logger';
 
+const MODULE_NOT_FOUND = 'MODULE_NOT_FOUND';
+
 export function tryRequire(path, root) {
   assert(path, 'Need a path to require');
-  log.verb(`Requiring ${path}`);
-  const errs = [];
-  for (let [err, mod] of[
-      requireNative(path),
-      requireFromRoot(path, root),
-      resolveFromRoot(path, root)
-    ]) {
-    if (mod) return mod;
-    errs.push(err);
+  log.verb(`Requiring '${path}'...`);
+  const ret = requireNative(path)
+    || resolveFromRoot(path, root)
+    || requireFromRoot(path, root);
+  if (ret) {
+    return ret;
+  } else {
+    const err = new Error(`Cannot find module '${path}' from '${root}'`);
+    err.code = MODULE_NOT_FOUND;
+    throw err;
   }
-
-  const error = new Error(
-    `Couldn't require {path: ${path}} from {root: ${root}}.`
-    // + errs.reduce( ( m, e ) => m + '\n\t' + e.message, '' )
-  );
-  error.errors = errs;
-  throw error;
 }
 
 export function requireNative(path) {
-  log.silly(`requireNative: {path: ${path}}`);
+  log.silly(`requireNative: {path: '${path}'}`);
   try {
-    return [null, module.require(path)];
-  } catch (error) {
-    error.message = 'requireNative: ' + error.message;
-    return [error];
-  }
-}
-
-export function requireFromRoot(path, root) {
-  assert(root, 'Need a root path to require from');
-  log.silly(`requireFromRoot: {path: ${path}} from {root: ${root}}`);
-  try {
-    return [null, reqFrom(root, path)];
-  } catch (error) {
-    error.message = 'requireFromRoot: ' + error.message;
-    return [error];
+    return module.require(path);
+  } catch (err) {
+    if (err.code === MODULE_NOT_FOUND) {
+      return false;
+    } else {
+      throw err;
+    }
   }
 }
 
 export function resolveFromRoot(path, root) {
   assert(root, 'Need a root path to require from');
-  log.silly(`resolveFromRoot: {path: ${path}} from {root: ${root}}`);
+  const reqPath = join(root, path);
+  log.silly(`resolveFromRoot: '${reqPath}'`);
   try {
-    return [null, module.require(resolve(root, path))];
-  } catch (error) {
-    error.message = 'resolveFromRoot: ' + error.message;
-    return [error];
+    return module.require(reqPath);
+  } catch (err) {
+    const erRex = path
+    .replace(/^[./\\]+/, '')
+    .replace(/[\\/]/g, '[\\\\/]');
+    if (err.code === MODULE_NOT_FOUND && err.message.match(erRex)) {
+      return false;
+    } else {
+      err.message = `Error in '${path}': ${err.message}`;
+      throw err;
+    }
   }
 }
+
+export function requireFromRoot(path, root) {
+  assert(root, 'Need a root path to require from');
+  log.silly(`requireFromRoot: {path: '${path}'} from {root: '${root}'}`);
+  try {
+    return reqFrom(root, path);
+  } catch (err) {
+    if (err.message === 'missing path') {
+      return false;
+    } else {
+      err.message = `Error in '${path}': ${err.message}`;
+      throw err;
+    }
+  }
+}
+
